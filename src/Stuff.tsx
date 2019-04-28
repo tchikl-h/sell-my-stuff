bdc601c925c72bd049e45113fb52add13fbf2af2
Hervé <herve@loukoum.io>
2019-04-28 19:12:39 +0200

    edit price phone

--- src/Stuff.tsx
+++ src/Stuff.tsx
@@ -4,6 +4,7 @@ import Gallery from './Gallery';
 import SplitText from 'react-pose-text';
 import posed from 'react-pose';
 import NavBar from './NavBar';
+import Button from '@material-ui/core/Button';
 
 export default class Stuff extends React.Component<any> {
   state = {fontSize: 144, changeOpacity: false, isMobile: false}
@@ -78,7 +79,7 @@ export default class Stuff extends React.Component<any> {
       </div>
       <div className="pixelFont" style={{fontSize: `${this.state.fontSize / 1.5}px`, position: "absolute", top: "30%", textAlign: "center", width: "100%"}}>
         <SplitText pose={this.state.changeOpacity ? "visible" : "invisible"} charPoses={charPoses}>
-          Sell this smartphone for more than 1500 SEK and keep the change
+          Sell this smartphone for more than $100 and keep the change
         </SplitText>
       </div>
       <div style={{position: "absolute", top: "50%", left: "50%", transform: "translateX(-50%)", width: this.state.isMobile ? "90%" : `${window.innerHeight * 0.664}px`}}>
